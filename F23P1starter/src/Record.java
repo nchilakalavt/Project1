2a870677e2a61540d26a587d6a99900e76cf723a
prathamCVT <143216956+prathamCVT@users.noreply.github.com>
2023-09-11 11:43:32 -0400

    Add files via upload

--- F23P1starter/src/Record.java
+++ F23P1starter/src/Record.java
@@ -1,10 +1,18 @@
 public class Record {
     public int id;
     public Seminar S;
-
-    public Record(Seminar s) {
-           S = s;
+    public int length;
+    public int pos;
+    public Record(Seminar s,int p) {
+          try {
+            length = s.serialize().length;
+        }
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
           id = s.getID();
+          pos = p;
     }
 
     public int getID() {
