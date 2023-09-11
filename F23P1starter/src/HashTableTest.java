4632c422355c9864551cbf1603b27d271978a25d
prathamCVT <143216956+prathamCVT@users.noreply.github.com>
2023-09-11 11:24:16 -0400

    Add files via upload

--- F23P1starter/src/HashTableTest.java
+++ F23P1starter/src/HashTableTest.java
@@ -1,45 +1,50 @@
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import  student.TestCase;
+import student.TestCase;
 
-public class HashTableTest extends TestCase{
-    String[] keywords = {"Good", "Bad", "Ugly"};
+public class HashTableTest extends TestCase {
+    String[] keywords = { "Good", "Bad", "Ugly" };
     Seminar mysem = new Seminar(1729, "Seminar Title", "2405231000", 75,
         (short)15, (short)33, 125, keywords, "This is a great seminar");
     Seminar mysem2 = new Seminar(1739, "Seminar Title", "2405231000", 75,
         (short)15, (short)33, 125, keywords, "This is a great seminar");
-    Record r = new Record(mysem); // id = 1729
-    Record r2 = new Record(mysem2); // id = 1739
+    Record r = new Record(mysem, 2); // id = 1729
+    Record r2 = new Record(mysem2, 3); // id = 1739
     HashTable h = new HashTable(2);
     HashTable h2 = new HashTable(1);
-    
+
     public void testInsert() {
         Seminar tester = new Seminar(1, "Seminar Title", "2405231000", 75,
             (short)15, (short)33, 125, keywords, "This is a great seminar");
-       Record testr = new Record(tester);
-       HashTable testhash = new HashTable(10);
-       testhash.hashInsert(r);
-       testhash.hashInsert(r2);
-       h2.hashInsert(r);
-       h2.hashInsert(r2);
-       
+        Record testr = new Record(tester, 3);
+        HashTable testhash = new HashTable(10);
+        testhash.hashInsert(r);
+        testhash.hashInsert(r2);
+        h2.hashInsert(r);
+        h2.hashInsert(r2);
+
     }
+
+
     public void testSearch() {
         assertEquals(h.search(1739), null);
         h.hashInsert(r2);
         h2.hashInsert(r);
-        assertEquals(h.search(1739),r2);
+        assertEquals(h.search(1739), r2);
         assertEquals(h2.search(1729), r);
         h.delete(r2);
         assertEquals(h.search(189), null);
     }
+
+
     public void testGetSize() {
-        assertEquals(h2.getSize(),1);
+        assertEquals(h2.getSize(), 1);
         h2.hashInsert(r);
-        assertEquals(h2.getSize(),2);
+        assertEquals(h2.getSize(), 2);
         h2.hashInsert(r2);
-        assertEquals(h2.getSize(),4);
+        assertEquals(h2.getSize(), 4);
     }
+
 
     public void testDelete() {
         HashTable h3 = new HashTable(4);
@@ -48,7 +53,4 @@ public class HashTableTest extends TestCase{
         assertEquals(h3.search(1729), null);
         assertEquals(h3.delete(r2), "Record with ID 1739 not found.");
     }
-
-
-
 }
