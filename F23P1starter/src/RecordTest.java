f0469bf6a93a310f5488b28458a38afbb74ee99d
prathamCVT <143216956+prathamCVT@users.noreply.github.com>
2023-09-17 15:12:33 -0400

    Add files via upload

---
+++ F23P1starter/src/RecordTest.java
@@ -0,0 +1,24 @@
+import student.TestCase;
+
+public class RecordTest extends TestCase {
+    /**
+     * Test method for testing the record class
+     */
+    Handle han = new Handle(20, 20);
+
+    public void testRecord() {
+        Record r = new Record(han, 10);
+        assertEquals(r.getHandle(), han);
+        assertEquals(r.getID(), 10);
+        r.setID(12);
+        assertEquals(12, r.getID());
+    }
+    /*
+     * Test method for Handle Class, which record takes in
+     */
+    public void testHandle() {
+        assertEquals(han.getStartPos(), 20);
+        assertEquals(han.getEndPos(), 20);
+        assertEquals(han.getLength(), 0);
+    }
+}
